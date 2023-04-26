3144fdcc3c4475b6657530c6b70f1ca4151be98b
AlricPerso <alric.conte@gmail.com>
2023-04-26 18:15:54 +0200

    protect malloc

--- mod01/ex01/main.cpp
+++ mod01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ale-cont <ale-cont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 17:44:28 by ale-cont          #+#    #+#             */
-/*   Updated: 2023/04/26 18:10:18 by ale-cont         ###   ########.fr       */
+/*   Updated: 2023/04/26 18:15:29 by ale-cont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ int main(void) {
 	int numZombies = 10;
 	std::string zombieName = "Alix";
 	Zombie *horde = zombieHorde(numZombies, zombieName);
-
+	if (horde == NULL)
+		return NULL;
 	for (int i = 0; i < numZombies; i++)
 		horde[i].announce();
 
