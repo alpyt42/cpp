393d01b70dd77415035b57f0972ef3498ff2a74d
AlricPerso <alric.conte@gmail.com>
2023-04-27 14:51:20 +0200

    exo02 done

--- mod01/ex02/main.cpp
+++ mod01/ex02/main.cpp
@@ -6,11 +6,24 @@
 /*   By: ale-cont <ale-cont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 18:18:47 by ale-cont          #+#    #+#             */
-/*   Updated: 2023/04/26 18:19:29 by ale-cont         ###   ########.fr       */
+/*   Updated: 2023/04/27 14:47:19 by ale-cont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
 int	main(void)
 {
+	std::string	brain = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &brain;
+	std::string&	stringREF = brain;
 	
+	std::cout << "The address of the string in memory : " << &brain << std::endl;
+	std::cout << "The address stored in stringPTR : " << stringPTR << std::endl;
+	std::cout << "The address stored in stringREF : " << &stringREF << std::endl;
+	std::cout << "The value of the string : " << brain << std::endl;
+	std::cout << "The value pointed to by stringPTR : " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF : " << stringREF << std::endl;
+
+	return (0);
 }
