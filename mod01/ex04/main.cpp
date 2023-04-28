0d2644f90b0743b29df188bd0f9228713d2c9fdb
AlricPerso <alric.conte@gmail.com>
2023-04-28 14:55:29 +0200

    up

--- mod01/ex04/main.cpp
+++ mod01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ale-cont <ale-cont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 18:01:10 by ale-cont          #+#    #+#             */
-/*   Updated: 2023/04/27 19:15:44 by ale-cont         ###   ########.fr       */
+/*   Updated: 2023/04/28 14:03:49 by ale-cont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,57 +14,86 @@
 #include <fstream>
 #include <string>
 
-// void	replacestrings(std::ifstream ifs, std::ofstream ofs, char **argv) {
-	
-// }
-
-int main(int argc, char **argv)
-{
-	if (argc != 4)
-	{
+bool checkArguments(int argc, char **argv) {
+	if (argc != 4) {
 		std::cerr << "Please provide <filename> <string1> <string2>" << std::endl;
-		return 1;
+		return false;
 	}
 
-	std::ifstream ifs(argv[1]);
-	if (!ifs.is_open())
-	{
+	if (std::string(argv[2]).length() == 0) {
+		std::cerr << "Please provide a string non NULL to find/replace." << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+bool openFiles(std::ifstream& ifs, std::ofstream& ofs, char **argv) {
+	ifs.open(argv[1]);
+	if (!ifs.is_open()) {
 		std::cerr << "Error: could not open input file '" << argv[1] << "'" << std::endl;
-		return 1;
+		return false;
 	}
 
 	std::string	fd_output = std::string(argv[1]) + ".replace";
-	std::ofstream ofs(fd_output.c_str());
-	if (!ofs.is_open())
-	{
+	ofs.open(fd_output.c_str());
+	if (!ofs.is_open()) {
 		std::cerr << "Error: could not open output file '" << fd_output << "'" << std::endl;
-		return 1;
+		return false;
 	}
+
+	return true;
+}
+
+int findAndReplace(std::ifstream& ifs, std::ofstream& ofs, char **argv) {
 	std::string	line;
 	std::string	newline;
 	size_t		pos;
 	size_t		start_pos;
+	size_t		count = 0;
 	while (std::getline(ifs, line)) {
 		start_pos = 0;
+		newline = "";
 		while (true) {
 			pos = line.find(argv[2], start_pos);
 			if (pos == std::string::npos) {
 				newline += line.substr(start_pos);
-				break ;
-			}
-			else {
+				break;
+			} else {
 				newline += line.substr(start_pos, pos - start_pos);
 				newline += argv[3];
 				start_pos = pos + std::string(argv[2]).length();
+				count++;
 			}
 		}
 		ofs << newline << std::endl;
 	}
+	return count;
+}
+
+int main(int argc, char **argv) {
+	int	count;
+	std::ifstream ifs;
+	std::ofstream ofs;
+
+	if (!checkArguments(argc, argv))
+		return 1;
+
+	if (!openFiles(ifs, ofs, argv))
+		return 1;
+
+	count = findAndReplace(ifs, ofs, argv);
 	ifs.close();
 	ofs.close();
 
-	std::cout << "File has been copied and pasted to '" << fd_output << "' and \n'"
-				<< argv[2] << "' portion(s) was/were successfully replaced by '" << argv[3]
-				<< "'." << std::endl;
+	if (count)
+		std::cout << "File has been copied and pasted to '" << argv[1] << ".replace"
+			<< "' and \n" << count << " portion(s) of '"
+			<< argv[2] << "' was/were successfully replaced by '" << argv[3] << "'." << std::endl;
+	else
+		std::cout << "File has been copied and pasted to '" << argv[1] << ".replace"
+			<< "' but no correspondance of '" << argv[2]
+			<< "' has been found in the input file." << std::endl;
+
 	return 0;
 }
